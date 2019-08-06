d733a2d13a066461b437b05f7cde563a6e8123cf
Asha <ashalbenjamin@gmail.com>
2019-08-05 21:03:26 -0500

    updated-it-now-shows

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -7,7 +7,6 @@ import './App.css';
 const App = () => {
   const [characterList, setCharacterList] = useState([]);
 
-
   useEffect( () => {
     axios
     .get('https://swapi.co/api/people')
@@ -16,11 +15,6 @@ const App = () => {
       setCharacterList(res.data.results)
     })
   }, [])
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-const App = () => {
-
   return (
     <div className="App">
       <Header as="h1" className="Header">React Wars</Header>
@@ -28,4 +22,4 @@ const App = () => {
     </div>
   );
 }
-// export default App;
+export default App
